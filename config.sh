3809e969e4168bab18bd3136784bdf7227f8c2f5
Sean Bryan <sean.bryan@anu.edu.au>
2024-10-11 14:59:06 +1100

    Move input datasets outside of model repo

--- config.sh
+++ config.sh
@@ -1,10 +1,10 @@
 # Use shell parameter expansion for runtime configurability - see
 # https://www.gnu.org/software/bash/manual/html_node/Shell-Parameter-Expansion.html:
-: "${CABLE_REPO:=${HOME}/cable}"
+: "${CABLE_INPUTS:=${HOME}/cable_inputs}"
 : "${EXE:=${HOME}/cable/bin/cable}"
 
 INPUT=(
-    "${CABLE_REPO}/src/offline/TumbaFluxnet.1.3_met.nc"
-    "${CABLE_REPO}/src/offline/gridinfo_CSIRO_1x1.nc"
+    "${CABLE_INPUTS}/forcing/TumbaFluxnet.1.3_met.nc"
+    "${CABLE_INPUTS}/gridinfo/gridinfo_CSIRO_1x1.nc"
 )
 
