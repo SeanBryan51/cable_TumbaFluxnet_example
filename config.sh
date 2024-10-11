af6180cd493becd02751868d87b80c47dd6c4d70
Sean Bryan <sean.bryan@anu.edu.au>
2024-10-11 11:49:47 +1100

    Replace input paths with environment variable

--- config.sh
+++ config.sh
@@ -1,8 +1,10 @@
-INPUT=(
-    '/Users/seanbryan/dev_local/cable/src/offline/TumbaFluxnet.1.3_met.nc'
-    '/Users/seanbryan/dev_local/cable/src/offline/gridinfo_CSIRO_1x1.nc'
-)
-
 # Use shell parameter expansion for runtime configurability - see
 # https://www.gnu.org/software/bash/manual/html_node/Shell-Parameter-Expansion.html:
-: "${EXE:=/Users/seanbryan/dev_local/cable/bin/cable}"
+: "${CABLE_REPO:=${HOME}/cable}"
+: "${EXE:=${HOME}/cable/bin/cable}"
+
+INPUT=(
+    "${CABLE_REPO}/src/offline/TumbaFluxnet.1.3_met.nc"
+    "${CABLE_REPO}/src/offline/gridinfo_CSIRO_1x1.nc"
+)
+
